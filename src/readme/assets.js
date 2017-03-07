bf7e04f0025a298c232e40abb48b793e43ea8dc5
Sébastien Elet <sebastien@elet.fr>
2017-03-07 22:20:12 +0100

    style(readme): fix linting

--- src/readme/assets.js
+++ src/readme/assets.js
@@ -7,57 +7,57 @@ module.exports = (file, packageConf, { PROJECT_DIR, glob, fs, log }) => {
   // Add www to ignored files
   if('README.md' === file.name) {
     // Header
-    file.data += '# ' + packageConf.name + '\n';
+    file.data += `# ${packageConf.name}\n`;
     if(packageConf.description) {
-      file.data += '> ' + packageConf.description + '\n';
+      file.data += `> ${packageConf.description}\n`;
     }
     file.data += '\n';
 
     // Badges
-    file.data += '[![NPM version](https://badge.fury.io/js/' +
-      packageConf.name + '.svg)](https://npmjs.org/package/' +
-      packageConf.name + ')\n';
-    file.data += '[![Build status](https://secure.travis-ci.org/' +
-      USERNAME + '/' + packageConf.name +
-      '.svg)](https://travis-ci.org/' +
-      USERNAME + '/' + packageConf.name +
-      ')\n';
-    file.data += '[![Dependency Status](https://david-dm.org/' +
-      USERNAME + '/' + packageConf.name +
-      '.svg)](https://david-dm.org/' +
-      USERNAME + '/' + packageConf.name +
-      ')\n';
-    file.data += '[![devDependency Status](https://david-dm.org/' +
-      USERNAME + '/' + packageConf.name +
-      '/dev-status.svg)](https://david-dm.org/' +
-      USERNAME + '/' + packageConf.name +
-      '#info=devDependencies)\n';
+    file.data += `[![NPM version](https://badge.fury.io/js/${
+      packageConf.name}.svg)](https://npmjs.org/package/${
+      packageConf.name})\n`;
+    file.data += `[![Build status](https://secure.travis-ci.org/${
+      USERNAME}/${packageConf.name
+      }.svg)](https://travis-ci.org/${
+      USERNAME}/${packageConf.name
+      })\n`;
+    file.data += `[![Dependency Status](https://david-dm.org/${
+      USERNAME}/${packageConf.name
+      }.svg)](https://david-dm.org/${
+      USERNAME}/${packageConf.name
+      })\n`;
+    file.data += `[![devDependency Status](https://david-dm.org/${
+      USERNAME}/${packageConf.name
+      }/dev-status.svg)](https://david-dm.org/${
+      USERNAME}/${packageConf.name
+      }#info=devDependencies)\n`;
     if(packageConf.devDependencies.coveralls) {
-      file.data += '[![Coverage Status](https://coveralls.io/repos/' +
-        USERNAME + '/' + packageConf.name +
-        '/badge.svg?branch=master)](https://coveralls.io/r/' +
-        USERNAME + '/' + packageConf.name +
-        '?branch=master)\n';
+      file.data += `[![Coverage Status](https://coveralls.io/repos/${
+        USERNAME}/${packageConf.name
+        }/badge.svg?branch=master)](https://coveralls.io/r/${
+        USERNAME}/${packageConf.name
+        }?branch=master)\n`;
     }
-    file.data += '[![Code Climate](https://codeclimate.com/github/' +
-      USERNAME + '/' + packageConf.name +
-      '.svg)](https://codeclimate.com/github/' +
-      USERNAME + '/' + packageConf.name +
-      ')\n';
-    file.data += '[![Dependency Status](https://dependencyci.com/github/' +
-      USERNAME + '/' + packageConf.name +
-      '/badge)](https://dependencyci.com/github/' +
-      USERNAME + '/' + packageConf.name +
-      ')\n';
+    file.data += `[![Code Climate](https://codeclimate.com/github/${
+      USERNAME}/${packageConf.name
+      }.svg)](https://codeclimate.com/github/${
+      USERNAME}/${packageConf.name
+      })\n`;
+    file.data += `[![Dependency Status](https://dependencyci.com/github/${
+      USERNAME}/${packageConf.name
+      }/badge)](https://dependencyci.com/github/${
+      USERNAME}/${packageConf.name
+      })\n`;
     return Promise.all([
       file.data,
       _getReadmeContents({ PROJECT_DIR, fs, log }),
       _getAPIContents({ PROJECT_DIR, fs, log }),
     ]).then((chunks) => {
-      file.data = chunks.filter(_identity).join('\n') + '\n';
-      file.data += '# License\n' +
-      '[' + packageConf.license + '](https://github.com/' +
-      USERNAME + '/' + packageConf.name + '/blob/master/LICENSE)\n';
+      file.data = `${chunks.filter(_identity).join('\n')}\n`;
+      file.data += `${'# License\n' +
+      '['}${packageConf.license}](https://github.com/${
+      USERNAME}/${packageConf.name}/blob/master/LICENSE)\n`;
       return file;
     });
   }
